8118170b8af4762944f7d38347904c873d96f511
Tennison Capra <tennison_capra@hotmail.com>
2022-11-13 21:27:46 -0300

    finishing 4 operations logic

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #0AF;
+    color: #FFF;
+    font-size: 24px;
+    border-radius: 10px;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #0A5;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    margin-bottom: 1px;
+
+    input {
+        padding: 5px;
+        width: 100%;
+        height: 65px;
+        background-color: #0A5;
+        border: 0;
+        text-align: right;
+        font-size: 26px;
+        font-family: 'Roboto';
+        color: #FFF;
+    }
+`
